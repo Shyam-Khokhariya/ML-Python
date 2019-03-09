019d3b5e4dba2aca4381eb675dfd1832cec9dd59
shyamkokhariya <40089839+Shyam-Khokhariya@users.noreply.github.com>
2019-03-09 17:33:51 +0530

    Add files via upload

---
+++ C05_KPCA_Modified.py
@@ -0,0 +1,43 @@
+from scipy.spatial.distance import pdist,squareform
+from scipy.linalg import eigh
+from scipy import exp
+import numpy as np
+
+def rbf_kernel_pca(X,gamma,n_component):
+    """
+
+        :param X: {numpy ndarray} shape: {n_sample,n_features}
+        :param gamma:float
+                     tuning parameter for RBF Kernel
+        :param n_component:int
+                     return number of principle component
+        :return:
+        X_pc: {numpy ndarray} shape:{n_sample,n_features}
+            Projected Dataset
+        """
+
+    # Calculate pairwise Euclidean Distance
+    sq_dis = pdist(X, 'sqeuclidean')
+
+    # Convert into square matrix
+    mat_sq_dis = squareform(sq_dis)
+
+    # Compute symmetric Kernel matrix
+    K = exp(-gamma * mat_sq_dis)
+
+    # Center of Kernel Matrix
+    N = K.shape[0]
+
+    one_np = np.ones((N, N)) / N
+    K = K - one_np.dot(K) - K.dot(one_np) + one_np.dot(K).dot(one_np)
+
+    # obtaining eigen pair from center kernel matrix
+    # scipy.linalg.eig return in ascending order
+    eigen_val, eigen_vec = eigh(K)
+    eigen_val, eigen_vec = eigen_val[::-1], eigen_vec[:, ::-1]
+
+    # collecting top k elements
+    alpha = np.column_stack((eigen_vec[:, i] for i in range(n_component)))
+    lambdas=[eigen_val[i] for i in range(n_component)]
+
+    return alpha,lambdas
